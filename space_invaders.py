414dfc91c85256bc51a43131f0a159282774d731
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-17 15:24:30 +0000

    Add files via upload
    added movement

--- space_invaders.py
+++ space_invaders.py
@@ -21,9 +21,12 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('001-spaceship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+rate = 0.3 #rate of change pos
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # the game loop (keeps window open until we quit) 
@@ -31,10 +34,22 @@ running = True
 while running:
      
     screen.fill((0,0,0)) #((R,G,B))
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-   
-    player() #player called after screen as player must be on top of screen.
+    
+        # if keystroke is pressed check whethere right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -rate
+            if event.key == pygame.K_RIGHT:
+                playerX_change = rate
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    player(playerX, playerY) #player called after screen as player must be on top of screen.
     pygame.display.update()
