0e853b5ccd150fff612228469abdf039ae5eb0e5
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-21 16:48:26 +0000

    Add files via upload
    I have finished the basic setup of the game. It takes 7 inputs (6 components and one target number) and then reprints and asks if they are correct. If the answer is no then we repeat the setup process.

--- countdown_numbers.c
+++ countdown_numbers.c
@@ -6,30 +6,35 @@ int main(){
   int target;
   int chose_numbers[6];
   int loop;
-  char operations[] = {\+, \-, \/, \*};
+  int proof = 0;
+  // using numbers to corrispond to operations
+  int operations[] = {1, 2, 3, 4};
 
-  printf("What is your first number \n");
-  scanf("%d", &chose_numbers[0]);
-  printf("What is your second number \n");
-  scanf("%d", &chose_numbers[1]);
-  printf("What is your third number \n");
-  scanf("%d", &chose_numbers[2]);
-  printf("What is your fourth number \n");
-  scanf("%d", &chose_numbers[3]);
-  printf("What is your fifth number \n");
-  scanf("%d", &chose_numbers[4]);
-  printf("What is your sixth number \n");
-  scanf("%d", &chose_numbers[5]);
-  printf("What is your target \n");
-  scanf("%d", &target);
+  while(proof == 0){
+    // asking user for the input numbers and target
+    printf("What is your first number \n");
+    scanf("%d", &chose_numbers[0]);
+    printf("What is your second number \n");
+    scanf("%d", &chose_numbers[1]);
+    printf("What is your third number \n");
+    scanf("%d", &chose_numbers[2]);
+    printf("What is your fourth number \n");
+    scanf("%d", &chose_numbers[3]);
+    printf("What is your fifth number \n");
+    scanf("%d", &chose_numbers[4]);
+    printf("What is your sixth number \n");
+    scanf("%d", &chose_numbers[5]);
+    printf("What is your target \n");
+    scanf("%d", &target);
 
-  printf("Your numbers are: ");
-  for(loop = 0; loop < 6; loop++)
-    printf("%d ", chose_numbers[loop]);
-  printf("\nWith a target of %d\n", target);
-
-  for(loop = 0; loop < 4; loop ++)
-    printf("%s", operations[loop]);
+    // letting user proof read their numbers
+    printf("Your numbers are: ");
+    for(loop = 0; loop < 6; loop++)
+      printf("%d ", chose_numbers[loop]);
+    printf("\nWith a target of %d\n", target);
+    printf("Is this correct (1 = yes, 0 = no)? \n"); // would prefer y/n
+    scanf("%d", &proof);
+  }
 
   return 0;
 }
